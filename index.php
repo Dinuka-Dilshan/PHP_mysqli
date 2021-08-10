0cf20efcc65f7caf3df4e2e7e81fbf5f3f7f9101
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-08-10 12:42:18 +0530

    retrive data

--- index.php
+++ index.php
@@ -4,6 +4,64 @@
     if(!$connection){
         echo mysqli_connect_error();
     }else{
-        echo 'done';
+        //echo 'connected';
+
+        //create sql query
+        $query = 'SELECT * FROM users';
+
+        //execute the query on the server and get back the results
+        $result = mysqli_query($connection,$query);
+
+        //convert result object to an associative array(so we can access with the field name)
+        $users = mysqli_fetch_all($result,MYSQLI_ASSOC);
+        
+        foreach($users as $user){
+            echo "<p>".$user['password']."</p>";
+        }
+
     }
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    
+
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Username</th>
+      <th scope="col">Email</th>
+      <th scope="col">Mobile</th>
+      <th scope="col">NIC</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+
+</body>
+</html>
